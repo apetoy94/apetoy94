d9dacf6d2b9005d0274e43b7fdec5a0d6cd98593
APETOY-LENOVO\APETOY <apetoy94@naver.com>
2018-09-05 17:54:54 +0900

    test

---
+++ src/main/java/kr/co/apetoy94/Apetoy94Application.java
@@ -0,0 +1,12 @@
+package kr.co.apetoy94;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apetoy94Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apetoy94Application.class, args);
+	}
+}
